1de297f5b0a2787f97f786a4ddeaa674d7afae5a
JesusGarciaP <theskyevolution@gmail.com>
2020-06-10 01:04:12 -0500

    se agregaron los permisos al manifiesto para enviar sms, leer el estado del telefono, llamada telefonica y registro de llamadas y se creo la clase MiBroadcastReceiver con su respectivo metodo onReceive

--- app/src/main/java/com/jesusgarcia/broadcastreceiverytelefonia/MainActivity.java
+++ app/src/main/java/com/jesusgarcia/broadcastreceiverytelefonia/MainActivity.java
@@ -2,13 +2,26 @@ package com.jesusgarcia.broadcastreceiverytelefonia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText txtTelefono, txtMsg;
+static Intent i = new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtTelefono = findViewById(R.id.txtTel);
+        txtMsg = findViewById(R.id.txtMSG);
+    }
+
+    public void btnGuardar(View view) {
+        i.putExtra("tel",txtTelefono.getText().toString());
+        i.putExtra("msg",txtMsg.getText().toString());
+        Toast.makeText(this, "Boton presionado", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/jesusgarcia/broadcastreceiverytelefonia/MiBroadcastReceiver.java
@@ -0,0 +1,42 @@
+package com.jesusgarcia.broadcastreceiverytelefonia;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class MiBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        final String tel = MainActivity.i.getStringExtra("tel");
+        final String msg = MainActivity.i.getStringExtra("smg");
+
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
+
+        telephonyManager.listen(new PhoneStateListener() {
+            @Override
+            public void onCallStateChanged(int state, String phoneNumber) {
+                super.onCallStateChanged(state, phoneNumber);
+                if (state == TelephonyManager.CALL_STATE_RINGING) {
+                    if (phoneNumber.equals("+52" + tel)) {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(tel, null, msg, null, null);
+                        Toast.makeText(context, "Mensaje enviado.", Toast.LENGTH_LONG).show();
+                    } else if (phoneNumber.equals(tel)) {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(tel, null, msg, null, null);
+                        Toast.makeText(context, "Mensaje enviado.", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(context, "El numero ingresado: " +
+                                tel + "\nes diferente del numero entrante: " +
+                                phoneNumber, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        }, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+}
